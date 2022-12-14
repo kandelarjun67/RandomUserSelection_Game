421c824e3c867e2fdadebc46f50a06bcad83fa5f
kandelarjun67 <kandelarjun67@gmail.com>
2022-12-14 14:07:14 +0545

    cors and body-parser install

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+//cors
+const cors = require('cors')
+app.use(cors())
+
+//bodyParser() 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// mongoose & schema
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
