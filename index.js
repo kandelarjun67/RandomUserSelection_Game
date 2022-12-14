d2d22160ffb9b6ec28ade5b02de5b72d3c22c000
kandelarjun67 <kandelarjun67@gmail.com>
2022-12-14 14:16:39 +0545

    connect to mongodb database

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 // mongoose & schema
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+// schema creation 
+
+
+// connect to database
+const connect = async () => {
+    try {
+      await mongoose.connect('mongodb://127.0.0.1:27017/userSelection', { useNewUrlParser: true, useUnifiedTopology: true });
+      console.log("Connected to MongoDB");     // connect to database name userSelection
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  connect()
 
 
 app.get('/', (req, res) => {
